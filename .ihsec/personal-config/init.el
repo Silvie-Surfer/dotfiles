5de45262c840e821ae16660995727b1f71dc9b2f
Sylvia Rodriguez <systemsilvie@protonmail.com>
2022-06-23 07:25:08 -0700

    Add ihsec -- See Explanation.
    ihsec allows for easy switching between Emacs configs. However, due to not wanting to deal with git submodules and for the sake of cleanliness, only personal configs are allowed on this repo. Any non-personal configs must be handled on the respective local machine.

---
+++ .ihsec/personal-config/init.el
@@ -0,0 +1,47 @@
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(require 'xah-fly-keys)
+(xah-fly-keys-set-layout "dvorak")
+(xah-fly-keys 1)
+
+(define-key xah-fly-command-map (kbd "<backspace>") 'xah-fly-leader-key-map)
+
+(require 'which-key)
+(which-key-mode 1)
+
+(load-theme 'modus-vivendi t)
+
+;; Remove UI Elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(set-fringe-mode 10) ; Give breathing room from edge of screen
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(modus-themes use-package xah-fly-keys which-key)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
